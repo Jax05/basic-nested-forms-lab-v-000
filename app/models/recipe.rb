5475bf3371b8157746a48be3dee3685a5d837c47
Carley Tripp <carley.tripp@outlook.com>
2019-03-04 02:12:29 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,4 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
   accepts_nested_attributes_for :ingredients
-
-  # def ingredient_attributes=(ingredient_attributes)
-  #   ingredient_attributes.each do |i, ingredient_attribute|
-  #     self.ingredients.build(ingredient_attribute)
-  #   end
-  # end
 end
